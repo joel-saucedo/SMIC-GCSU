e8dff792ae1865a12d207fb0edf55fcb11816f90
joel-saucedo <134741691+joel-saucedo@users.noreply.github.com>
2025-02-23 17:07:43 -0500

    Update rolling_capm.py

--- stock_research/rolling_capm.py
+++ stock_research/rolling_capm.py
@@ -40,7 +40,7 @@ df_spy["return"] = df_spy["close"].pct_change()
 
 #------------------------------------------------------------------------------
 # Get the risk-free rate from FRED using the 10-Year T-Bond yield.
-fred = Fred(api_key="db4adf1cadf0ab552ea07e2c4f2bb8cf")
+fred = Fred(api_key="apikey")
 # Request risk-free data for the desired period
 rf_series = fred.get_series('DGS10', observation_start='2020-01-01', observation_end='2025-12-31')
 rf_series = rf_series.dropna().sort_index()
